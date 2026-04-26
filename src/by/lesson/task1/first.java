50f31bd35216e4368488ff8c3a61839377b558d9
Siarhej-I <goglepo4ta@gmil.com>
2026-04-26 14:23:56 +0300

    add class "First.class" -> commit

---
+++ src/by/lesson/task1/first.java
@@ -0,0 +1,8 @@
+package by.lesson.task1;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("Programmer: Ilchuk");
+        System.out.print("Date task1: 22 April 2026");
+    }
+}
