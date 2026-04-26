6dce299aaec8810876cf393afbf5c020f74e5047
Siarhej-I <goglepo4ta@gmil.com>
2026-04-26 16:05:38 +0300

    second commit

--- src/HW1/NameDate.java
+++ src/HW1/NameDate.java
@@ -1,6 +1,6 @@
-package by.lesson.task1;
+package HW1;
 
-public class first {
+public class NameDate {
     public static void main(String[] args) {
         System.out.println("Programmer: Ilchuk");
         System.out.print("Date task1: 22 April 2026");
